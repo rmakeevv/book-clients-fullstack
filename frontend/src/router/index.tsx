f33d4340e218601d34ae0b70dad98e64fab76f12
rmakee <r.makeevv@gmail.com>
2024-02-11 03:33:20 +0400

    auth page

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Root from './Root';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Auth from './Auth';
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+  },
+  {
+    path: '/auth',
+    element: <Auth />,
   },
 ]);
 
