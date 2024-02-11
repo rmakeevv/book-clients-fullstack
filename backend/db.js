d49170fa935fb50f701fd3adcb16225127e8b51c
rmakee <r.makeevv@gmail.com>
2024-02-11 08:04:58 +0400

    env

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,8 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 const { Pool } = pkg;
 
+dotenv.config();
+
 export const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
