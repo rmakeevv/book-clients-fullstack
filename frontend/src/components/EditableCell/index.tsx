22e83fddd4a58b7ee786623b2efe3068148b2f49
almakeev <r.makeevv@gmail.com>
2024-08-23 03:51:14 +0300

    prettier

--- frontend/src/components/EditableCell/index.tsx
+++ frontend/src/components/EditableCell/index.tsx
@@ -3,40 +3,40 @@ import React, { useMemo } from 'react';
 import { EditableCellProps } from './types';
 
 const EditableCell: React.FC<EditableCellProps> = ({
-  editing,
-  dataIndex,
-  title,
-  inputType,
-  record,
-  index,
-  children,
-  ...restProps
+    editing,
+    dataIndex,
+    title,
+    inputType,
+    record,
+    index,
+    children,
+    ...restProps
 }) => {
-  const inputNode = useMemo(
-    () => (inputType === 'number' ? <InputNumber /> : <Input />),
-    [inputType]
-  );
+    const inputNode = useMemo(
+        () => (inputType === 'number' ? <InputNumber /> : <Input />),
+        [inputType]
+    );
 
-  return (
-    <td {...restProps}>
-      {editing ? (
-        <Form.Item
-          name={dataIndex}
-          style={{ margin: 0 }}
-          rules={[
-            {
-              required: true,
-              message: `Please Input ${title}!`,
-            },
-          ]}
-        >
-          {inputNode}
-        </Form.Item>
-      ) : (
-        children
-      )}
-    </td>
-  );
+    return (
+        <td {...restProps}>
+            {editing ? (
+                <Form.Item
+                    name={dataIndex}
+                    style={{ margin: 0 }}
+                    rules={[
+                        {
+                            required: true,
+                            message: `Please Input ${title}!`,
+                        },
+                    ]}
+                >
+                    {inputNode}
+                </Form.Item>
+            ) : (
+                children
+            )}
+        </td>
+    );
 };
 
 export default EditableCell;
