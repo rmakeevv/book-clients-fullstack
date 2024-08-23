64bfc0c68d653bb36275056c4045a43b2c743d7f
almakeev <r.makeevv@gmail.com>
2024-08-23 03:45:30 +0300

    db .env

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,6 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user: 'postgres',
-  password: 'dbpass',
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
