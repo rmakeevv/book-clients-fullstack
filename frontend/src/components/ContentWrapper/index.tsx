5f621016e345fa229ce239bf7e1a7821c86c2b50
almakeev <r.makeevv@gmail.com>
2024-08-23 03:48:34 +0300

    prettier

--- frontend/src/components/ContentWrapper/index.tsx
+++ frontend/src/components/ContentWrapper/index.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 import styles from './index.module.css';
 
 const ContentWrapper = ({ children }: PropsWithChildren) => {
-  return <div className={styles.container}>{children}</div>;
+    return <div className={styles.container}>{children}</div>;
 };
 
 export default ContentWrapper;
